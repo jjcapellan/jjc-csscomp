fa9b3d9b93926c25b2e4eb5f0a8775e110fd2b6b
jjcapellan <soycape@hotmail.com>
2019-12-11 21:27:59 +0100

    First release

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/jjcapellan/jjc-csscomp
+
+go 1.13
---
+++ main.go
@@ -0,0 +1,163 @@
+package csscomp
+
+import (
+	"fmt"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+func minColor(hexCol string) string {
+	if hexCol[1] == hexCol[2] && hexCol[3] == hexCol[4] && hexCol[5] == hexCol[6] {
+		return "#" + string(hexCol[1]) + string(hexCol[3]) + string(hexCol[5])
+	}
+	return hexCol
+}
+
+func quitQuotes(str string) string {
+	str = strings.ReplaceAll(str, "\"", "")
+	return str
+}
+
+func quitQuoteIfNoSpace(str string) string {
+	if strings.Index(str, " ") == -1 {
+		return strings.ReplaceAll(str, "\"", "")
+	}
+	return str
+}
+
+func quitffQuotes(str string) string {
+	rgx := regexp.MustCompile(`[:\,]\"[^\"]*\"[\,;]`)
+	str = rgx.ReplaceAllStringFunc(str, quitQuoteIfNoSpace)
+	return str
+}
+
+func intToHex(str string) string {
+	number, _ := strconv.Atoi(str)
+	strHex := fmt.Sprintf("%x", number)
+	if len(strHex) == 1 {
+		strHex = "0" + strHex
+	}
+	return strHex
+}
+
+func rgbToHex(str string) string {
+	str = strings.ReplaceAll(str, " ", "")
+	str = strings.Split(str, "(")[1]
+	str = strings.TrimRight(str, ")")
+	strArr := strings.Split(str, ",")
+	str = ""
+	for i := range strArr {
+		str += intToHex(strArr[i])
+	}
+	return "#" + str
+}
+
+// Removes zero units sufix
+func rmZeroSufix(str string) string {
+	rgx := regexp.MustCompile(`\s0[^0-9\s\;\,\.]{1,2}`)
+	str = rgx.ReplaceAllString(str, " 0")
+
+	rgx = regexp.MustCompile(`:0[^0-9\s\;\,\.]{1,2}`)
+	str = rgx.ReplaceAllString(str, ":0")
+
+	return str
+}
+
+// Deletes spaces for one match
+func rmSpacesFunc(str string) string {
+	if string(str[0]) == " " {
+		str = string(str[len(str)-1])
+	} else {
+		str = string(str[0])
+	}
+	return str
+}
+
+// Deletes spaces for all text
+func rmSpaces(str string) string {
+	rgx := regexp.MustCompile(`\s+[\{\}\,;\=:\)\(\]\[\+]`)
+	str = rgx.ReplaceAllStringFunc(str, rmSpacesFunc)
+
+	rgx = regexp.MustCompile(`[\{\}\,;\=:\)\(\]\[\+]\s+`)
+	str = rgx.ReplaceAllStringFunc(str, rmSpacesFunc)
+
+	rgx = regexp.MustCompile(`\s\s+`)
+	str = rgx.ReplaceAllString(str, " ")
+
+	return str
+}
+
+// Deletes all comments
+func rmComments(str string) string {
+	rgx := regexp.MustCompile(`url\s*\(\s*\"[^\"]*\"\s*\)`)
+	str = rgx.ReplaceAllStringFunc(str, quitQuotes)
+	return str
+}
+
+// Deletes all line jumps
+func rmJumps(str string) string {
+	rgx := regexp.MustCompile(`[\n]`)
+	str = rgx.ReplaceAllString(str, "")
+	return str
+}
+
+// Deletes begin line spaces
+func rmBeginSpaces(str string) string {
+	rgx := regexp.MustCompile(`^\s*`)
+	str = rgx.ReplaceAllString(str, "")
+	return str
+}
+
+// Deletes leading zeros
+func rmLeadingZeros(str string) string {
+	rgx := regexp.MustCompile(`[^0-9]0\.`)
+	str = rgx.ReplaceAllString(str, " .")
+	return str
+}
+
+// Deletes quotes when possible
+func rmQuotes(str string) string {
+	rgx := regexp.MustCompile(`type\=\"[^\"]*\"`)
+	str = rgx.ReplaceAllStringFunc(str, quitQuotes)
+
+	rgx = regexp.MustCompile(`font-family:[^;]*;`)
+	str = rgx.ReplaceAllStringFunc(str, quitffQuotes)
+
+	return str
+}
+
+// Converts rgb values to hexadecimal notation
+func rmRgb(str string) string {
+	rgx := regexp.MustCompile(`rgb\s*\([^,]+,[^,]+,[^,]+\)`)
+	str = rgx.ReplaceAllStringFunc(str, rgbToHex)
+	return str
+}
+
+// Converts hex colors to its shorthand
+func minColors(str string) string {
+	rgx := regexp.MustCompile(`\#[0-9a-fA-F]{6}`)
+	str = rgx.ReplaceAllStringFunc(str, minColor)
+	return str
+}
+
+// Get returns the minified css string
+func Get(txt string) string {
+	str := rmComments(txt)
+
+	str = rmJumps(str)
+
+	str = rmLeadingZeros(str)
+
+	str = rmSpaces(str)
+
+	str = rmZeroSufix(str)
+
+	str = rmQuotes(str)
+
+	str = rmRgb(str)
+
+	str = minColors(str)
+
+	return str
+}
